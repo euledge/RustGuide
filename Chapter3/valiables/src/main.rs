c4a82e9bea3d96792bb06f647ffee6b56011b242
euledge <hitoshi.kuroyanagi@gmail.com>
2019-02-12 23:40:08 +0900

    変数と定数

---
+++ Chapter3/valiables/src/main.rs
@@ -0,0 +1,12 @@
+fn main() {
+    const MAX_POINTS: u32 = 10_0000;
+    let mut x = 5;
+    println!("The value of x is : {}", x);
+    x = 6;
+    println!("The value of x is : {}", x);
+    println!("The value of MAX_POINTS is : {}", MAX_POINTS);
+
+    let spaces = "    ";
+    let spaces = spaces.len();   
+    println!("The value of x is : {}", spaces);
+}

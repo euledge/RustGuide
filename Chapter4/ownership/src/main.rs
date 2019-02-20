243632226df06bf8f3e61cba41fd15c3d7ef5292
euledge <hitoshi.kuroyanagi@gmail.com>
2019-02-21 00:34:54 +0900

    Chapter4 参照と借用 refs #8

--- Chapter4/ownership/src/main.rs
+++ Chapter4/ownership/src/main.rs
@@ -40,6 +40,32 @@ fn main() {
         makes_copy(x);
         println!("The Value is x: {}", x);
     }
+
+    // 4.1.7
+    {
+        let s = String::from("Hello World!");
+        let len = calculate_length(&s);
+        println!("{} length is {}", s, len);
+    }
+
+    {
+        let mut s = String::from("Hello!");
+        s = return_ownership(s);
+        println!("{}", s);
+    }
+
+    {
+        let s = String::from("Hello!");
+        let len = reference_ownership(&s);
+        println!("{} length is {}", s, len);
+        borrow_ownership(&s);
+    }
+
+    {
+        let mut s = String::from("Hello!");
+        borrow_mutable_ownership(&mut s);
+        println!("{}", s);
+    }
 }
 
 fn takes_ownership(some_string: String) {
@@ -48,4 +74,26 @@ fn takes_ownership(some_string: String) {
 
 fn makes_copy(number: i32) {
     println!("{}", number);
+}
+
+fn calculate_length(s: &String) -> usize {
+    //s.push_str("string: &str");
+    s.len()
+}
+
+fn return_ownership(some_string: String) -> String {
+    some_string
+}
+
+fn reference_ownership(some_string: &String) -> usize {
+    println!("referenced {}", some_string);
+    some_string.len()
+}
+
+fn borrow_ownership(some_string: &String)  {
+//    some_string.push_str(", World!");  
+}
+
+fn borrow_mutable_ownership(some_string: &mut String)  {
+    some_string.push_str(", World!");
 }
